961f35100338606b470b8e95aed45da138ecfae8
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-19 16:30:40 -0400

    Requirement 7 Completed! :heavy_check_mark: Select one color at a time

---
+++ script.js
@@ -0,0 +1,19 @@
+/* 
+Consultei o repositório da Gisele Costa para resolver o requisito nº 7
+link: https://github.com/tryber/sd-011-project-pixels-art/pull/104/commits/e828d37ace8a073bdea26e8b4c069f29c8eaf998#
+*/
+
+let colorPalette = document.getElementById("color-palette");
+
+colorPalette.addEventListener("click", selectColor);
+
+function selectColor(event) {
+    let eventTarget = event.target; //seleciona o evento alvo
+    let colors = document.getElementsByClassName('color'); //cria um HTML array com todas as minhas 'li's/cores
+    for (let index = 0; index < colors.length; index += 1) {
+        colors[index].classList.remove('selected'); //remove a cor selecionada antes de selecionar outra cor
+        if (eventTarget.className !== 'selected') { //se o meu evento alvo não tiver a classe 'selected' então...
+            eventTarget.classList.add('selected');//adiciona a classe no evento alvo/seleciona a nova cor
+        }
+    }
+}
