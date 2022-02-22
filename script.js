90e9b899bbc0fa4306a376169c0be213f8f639e1
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-22 12:08:44 -0400

    Requirement 8 Completed! :heavy_check_mark: Paint the pixel frame :art:

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ let colorPalette = document.getElementById("color-palette");
 colorPalette.addEventListener("click", selectColor);
 
 function selectColor(event) {
-    let eventTarget = event.target; //seleciona o evento alvo
-    let colors = document.getElementsByClassName('color'); //cria um HTML array com todas as minhas 'li's/cores
+    let eventTarget = event.target, //seleciona o evento alvo
+        colors = document.getElementsByClassName('color'); //cria um HTML array com todas as minhas 'li's/cores
     for (let index = 0; index < colors.length; index += 1) {
         colors[index].classList.remove('selected'); //remove a cor selecionada antes de selecionar outra cor
         if (eventTarget.className !== 'selected') { //se o meu evento alvo não tiver a classe 'selected' então...
             eventTarget.classList.add('selected');//adiciona a classe no evento alvo/seleciona a nova cor
         }
     }
+}
+
+let pixelBoard = document.getElementById("pixel-board");//chama o quadro de pixels
+
+pixelBoard.addEventListener("click", paintPixel);//ao clicar no meu quadro de pixels aciona um evento
+
+function paintPixel(event){
+    let eventTarget = event.target, //o evento que eu cliquei
+        selectedColor = document.getElementsByClassName("selected")[0].classList[1];//recebe a cor selecionada
+    eventTarget.style.backgroundColor = selectedColor;//atribui nova cor no pixel
 }
